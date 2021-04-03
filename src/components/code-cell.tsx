c32be6254a0d5461e3c6dfddc1d743e23727992e
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-04-03 23:42:49 +0500

    Resizable window and fixes in execution context

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,28 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CodeEditor from '../components/code-editor';
 import Preview from '../components/preview';
 import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
-    <div>
-      <CodeEditor
-        initialValue="//start coding here"
-        onChange={(value) => setInput(value as string)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
+    <Resizable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="//start coding here"
+            onChange={(value) => setInput(value as string)}
+          />
+        </Resizable>
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
